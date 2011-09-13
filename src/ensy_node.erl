e1a8d215c0240747b5bcbbe9c2914819c65fabb3
Enrico Franchi <efranchi@ce.unipr.it>
2011-09-13 18:31:40 +0200

    Started implementing the protocol to have node stubs customize the behavior of nodes.

--- src/ensy_node.erl
+++ src/ensy_node.erl
@@ -6,7 +6,9 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/0,
+         request_connection/1,
+         activate/2]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -14,6 +16,11 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
+-export([behaviour_info/1]).
+
+-define(request_connection_message(From, To), {request_connection, From, To}).
+
+-record(state, {stub, neighbours, stub_state}).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -22,18 +29,40 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+request_connection(Target) ->
+    Msg = ?request_connection_message(self(), Target),
+    case gen_server:call(Target, Msg) of
+        ok -> ok;
+        _ -> fail
+    end.
+
+activate(Node, Age) ->
+    gen_server:cast(Node, {activate, Age}).
+
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init({Stub, Init_Args}) ->
+    Opaque_State = Stub:init(Init_Args),
+    {ok, #state{stub=Stub, neighbours=sets:new(), stub_state=Opaque_State}}.
 
 handle_call(_Request, _From, State) ->
     {noreply, ok, State}.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+handle_cast({activate, Age}, #state{stub=Stub, stub_state=Opaque_State} = State) ->
+    {New_Stub, New_Opaque_State} = Stub:handle_activate(Opaque_State, Age),
+    {noreply, State#state{stub=New_Stub, stub_state=New_Opaque_State}};
+handle_cast(Msg, #state{stub=Stub, stub_state=Opaque_State} = State) ->
+    case Stub:handle_cast(Msg, Opaque_State) of
+        {noreply, {New_Stub, New_Opaque_State}} ->
+            {noreply, State#state{stub=New_Stub, stub_state=New_Opaque_State}};
+        {noreply, {New_Stub, New_Opaque_State, Timeout_Or_Hibernate}} ->
+            {noreply, State#state{stub=New_Stub, stub_state=New_Opaque_State},
+             Timeout_Or_Hibernate};
+        {stop, Reason, {New_Stub, New_Opaque_State}} ->
+            {stop, Reason, State#state{stub=New_Stub, stub_state=New_Opaque_State}}
+    end.
 
 handle_info(_Info, State) ->
     {noreply, State}.
@@ -43,6 +72,14 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+
+behaviour_info(callbacks) ->
+       [{init, 1},
+        {handle_cast, 2}
+        ];
+behaviour_info(_Other) ->
+    undefined.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
