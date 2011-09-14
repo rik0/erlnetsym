4104349748d5389d59f6cefd28381ad31fc45cbe
Enrico Franchi <efranchi@ce.unipr.it>
2011-09-14 19:41:41 +0200

    Minor formatting.

--- src/ensy_node.erl
+++ src/ensy_node.erl
@@ -7,18 +7,17 @@
 %% ------------------------------------------------------------------
 
 -export([start_link/0,
-         request_connection/1,
-         activate/2]).
+        request_connection/1,
+        activate/2]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+        terminate/2, code_change/3]).
 -export([behaviour_info/1]).
 
--define(request_connection_message(From, To), {request_connection, From, To}).
 
 -record(state, {stub, neighbours, stub_state}).
 
@@ -30,8 +29,7 @@ start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 request_connection(Target) ->
-    Msg = ?request_connection_message(self(), Target),
-    case gen_server:call(Target, Msg) of
+    case gen_server:call(Target, request_connection) of
         ok -> ok;
         _ -> fail
     end.
@@ -47,21 +45,47 @@ init({Stub, Init_Args}) ->
     Opaque_State = Stub:init(Init_Args),
     {ok, #state{stub=Stub, neighbours=sets:new(), stub_state=Opaque_State}}.
 
+handle_call(request_connection, From,
+    #state{stub=Stub,
+        stub_state=Opaque_State,
+        neighbours=Neighbours} = State) ->
+    case Stub:should_connect(Opaque_State, From) of
+        {true, New_Stub, New_Opaque_State} ->
+            {reply, ok,
+                State#state{
+                    stub=New_Stub,
+                    stub_state=New_Opaque_State,
+                    neighbours=sets:add_element(
+                        From, Neighbours)}};
+        {false, New_Stub, New_Opaque_State} ->
+            {reply, fail,
+                State#state{
+                    stub=New_Stub,
+                    stub_state=New_Opaque_State,
+                    neighbours=Neighbours}}
+    end;
 handle_call(_Request, _From, State) ->
     {noreply, ok, State}.
 
-handle_cast({activate, Age}, #state{stub=Stub, stub_state=Opaque_State} = State) ->
+handle_cast({activate, Age}, 
+    #state{stub=Stub, stub_state=Opaque_State} = State) ->
     {New_Stub, New_Opaque_State} = Stub:handle_activate(Opaque_State, Age),
     {noreply, State#state{stub=New_Stub, stub_state=New_Opaque_State}};
 handle_cast(Msg, #state{stub=Stub, stub_state=Opaque_State} = State) ->
     case Stub:handle_cast(Msg, Opaque_State) of
         {noreply, {New_Stub, New_Opaque_State}} ->
-            {noreply, State#state{stub=New_Stub, stub_state=New_Opaque_State}};
+            {noreply, State#state{
+                    stub=New_Stub, 
+                    stub_state=New_Opaque_State}};
         {noreply, {New_Stub, New_Opaque_State, Timeout_Or_Hibernate}} ->
-            {noreply, State#state{stub=New_Stub, stub_state=New_Opaque_State},
-             Timeout_Or_Hibernate};
+            {noreply, State#state{
+                    stub=New_Stub, 
+                    stub_state=New_Opaque_State},
+                Timeout_Or_Hibernate};
         {stop, Reason, {New_Stub, New_Opaque_State}} ->
-            {stop, Reason, State#state{stub=New_Stub, stub_state=New_Opaque_State}}
+            {stop, Reason, State#state{
+                    stub=New_Stub, 
+                    stub_state=New_Opaque_State}}
     end.
 
 handle_info(_Info, State) ->
@@ -75,9 +99,9 @@ code_change(_OldVsn, State, _Extra) ->
 
 
 behaviour_info(callbacks) ->
-       [{init, 1},
+    [{init, 1},
         {handle_cast, 2}
-        ];
+    ];
 behaviour_info(_Other) ->
     undefined.
 
