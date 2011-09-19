ccfb578ff811fe603a18395e7517fe5b4d43521b
Enrico Franchi <efranchi@ce.unipr.it>
2011-09-19 20:15:59 +0200

    Adding documentation to activator.

--- src/ensy_activator.erl
+++ src/ensy_activator.erl
@@ -3,6 +3,11 @@
 -define(SERVER, ?MODULE).
 -include("include/time.hrl").
 
+%% @headerfile "time.hrl"
+
+%% @type opaque_state(). The state used by the callback module.
+%% @type state() = {Stub::module(), opaque_state()}.
+%% `Stub' is the module where the callbacks are defined.
 -record(state, {stub, stub_state}).
 
 %% ------------------------------------------------------------------
@@ -25,26 +30,41 @@
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-
+% @spec start_link({stub_module, Module::module(), Init_Args::[atom()]}) -> Result
+% @doc Starts the nodes activator.
+% 
+% `Module' is an atom representing the module where the callbacks for
+% the activator are defined. `Init_Args' are passed to the stub module
+% init function.
+% @end
 start_link({stub_module, Module, Init_Args}) ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, 
-        {Module, Init_Args},
-        []).
+        {Module, Init_Args}, []).
 
+% @spec tick(Age::age()) -> ok
+% @doc Send a tick message to the node activator. The activator
+% uses the callbacks to determine which nodes to create, which nodes
+% to activate and which nodes to destroy. It is an error to call this
+% function from other than the clock.
 tick(Age) ->
     gen_server:cast(?SERVER, {tick, Age}).
 
+% @spec eow(Age::age()) -> Result
+% @doc Notifies the activator that the simulation ended. It is an error
+% to call this function from other than the clock.
 eow(Age) ->
     gen_server:call(?SERVER, {eow, Age}).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
-
+% @private
+% @spec init({Module::module(), Init_Args::[atom()]}) -> {ok, State, Timeout}
 init({Module, Init_Args}) ->
     Opaque_Stub_Handler = Module:init(Init_Args),
     {ok, #state{stub=Module, stub_state=Opaque_Stub_Handler}, 0}.
 
+% @private
 handle_cast({tick, Age}, #state{stub=Module, stub_state=Opaque_Stub_Handler} = State) ->
     {OSH1, To_Spawn} = Module:to_spawn(Opaque_Stub_Handler, Age),
     {OSH2, To_Activate} = Module:to_activate(OSH1, Age),
@@ -54,18 +74,23 @@ handle_cast({tick, Age}, #state{stub=Module, stub_state=Opaque_Stub_Handler} = S
     lists:map(fun destroy_node/1, To_Destroy),
     {noreply, State#state{stub_state=OSH3}}.
 
+% @private
 handle_call({eow, Age}, _From, State) ->
     {stop, normal, {ok, Age}, State}.
 
+% @private
 handle_info(_Request, State) ->
     {noreply, State}.
 
+% @private
 terminate(_Reason, _State) ->
     ok.
 
+% @private
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+% @private
 behaviour_info(callbacks) ->
        [{init, 1},
         {to_activate, 2},
@@ -78,11 +103,14 @@ behaviour_info(_Other) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
+% @private
 spawn_node({_Node_Module, Args}) ->
     ensy_nodes_sup:start_child(Args).
 
+% @private
 destroy_node(Node_Id) ->
     ensy_nodes_sup:terminate_child(Node_Id).
 
+% @private
 activate_node(_Node_Id) ->
     ok.
