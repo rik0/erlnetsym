d8711a6a3b9987d5b7c4cc0275a75bf9762a8342
Enrico Franchi <efranchi@ce.unipr.it>
2011-09-21 07:52:01 +0200

    Fixed run.erl script.

--- run.erl
+++ run.erl
@@ -3,13 +3,12 @@
 %% -*- erlang -*-
 %% -smp enable -pa ebin -boot start_sasl
 %% -smp enable -pa ebin 
-%%! -smp enable -pa ebin -boot start_sasl -sasl '{sasl_error_logger, {file, "/tmp/ensy.log"}'
+%%! -smp enable -pa ebin -boot start_sasl -sasl sasl_error_logger '{file, "/tmp/ensy.log"}'
 
 
 main(_Args) ->
     code:add_path("ebin"),
     application:start(sasl),
-    application:start(ensy),
+    application:start(ensy, permanent),
     lists:map(fun(App) -> io:format("~p~n", [App]) end, 
-        application:which_applications()),
-    io:get_chars("> ", 0).
+        application:which_applications()).
